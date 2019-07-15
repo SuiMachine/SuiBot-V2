27cdbe416556ff517c5e764d74efa2fecc32fcc4
SuicideMachine <SuiMachine@users.noreply.github.com>
2019-07-15 09:28:25 +0200

    Rat emoji

--- SuiBot-Core/Components/Other/RatsBirthday.cs
+++ SuiBot-Core/Components/Other/RatsBirthday.cs
@@ -10,7 +10,7 @@
                 var target = lastMessage.Message.Split(new char[] { ' ' }, 2)[1];
                 if (target != " ")
                 {
-                    channelInstance.SendChatMessageResponse(lastMessage, string.Format("Rats rats we are the rats - Celebrating yet another birthday bash! {0} it's your birthday today - Cake and Ice cream is on it's way! (and) {0} been such a good boy this year - Open up your gifts while we all cheer!",
+                    channelInstance.SendChatMessageResponse(lastMessage, string.Format("🐀 Rats rats we are the rats - Celebrating yet another birthday bash! {0} it's your birthday today - Cake and Ice cream is on it's way! (and) {0} been such a good boy this year - Open up your gifts while we all cheer! 🐀",
                         target), true);
                 }
                 else
