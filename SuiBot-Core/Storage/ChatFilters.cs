34a9796d13401e40b22a0996fcb422fd96e4e76f
SuiMachine <SuiMachine@users.noreply.github.com>
2024-01-13 23:44:27 +0100

    Clean this one up

--- SuiBot-Core/Storage/ChatFilters.cs
+++ SuiBot-Core/Storage/ChatFilters.cs
@@ -35,46 +35,24 @@ namespace SuiBot_Core.Storage
 
 		public static ChatFilters Load(string Channel)
 		{
-			string FilePath = string.Format("Bot/Channels/{0}/ChatFilters.xml", Channel);
-			ChatFilters obj;
-			if (File.Exists(FilePath))
+			string FilePath = $"Bot/Channels/{Channel}/ChatFilters.xml";
+			var newObj = XML_Utils.Load(FilePath, new ChatFilters()
 			{
-				XmlSerializer serializer = new XmlSerializer(typeof(ChatFilters));
-				FileStream fs = new FileStream(FilePath, FileMode.Open);
-				obj = (ChatFilters)serializer.Deserialize(fs);
-				fs.Close();
-				obj.Channel = Channel;
-
-				for (int i = 0; i < obj.PurgeFilters.Count; i++)
-				{
-					obj.PurgeFilters[i].CompiledSyntax = new Regex(obj.PurgeFilters[i].Syntax, RegexOptions.IgnoreCase | RegexOptions.Compiled);
-				}
-				for (int i = 0; i < obj.TimeOutFilter.Count; i++)
-				{
-					obj.TimeOutFilter[i].CompiledSyntax = new Regex(obj.TimeOutFilter[i].Syntax, RegexOptions.IgnoreCase | RegexOptions.Compiled);
-				}
-				for (int i = 0; i < obj.BanFilters.Count; i++)
-				{
-					obj.BanFilters[i].CompiledSyntax = new Regex(obj.BanFilters[i].Syntax, RegexOptions.IgnoreCase | RegexOptions.Compiled);
-				}
-
-				return obj;
-			}
-			else
-				return new ChatFilters() { Channel = Channel, BanFilters = new List<ChatFilter>(), TimeOutFilter = new List<ChatFilter>(), PurgeFilters = new List<ChatFilter>() };
+				BanFilters = new List<ChatFilter>(),
+				TimeOutFilter = new List<ChatFilter>(),
+				PurgeFilters = new List<ChatFilter>()
+			});
+			newObj.Channel = Channel;
+			for (int i = 0; i < newObj.PurgeFilters.Count; i++)
+				newObj.PurgeFilters[i].CompiledSyntax = new Regex(newObj.PurgeFilters[i].Syntax, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+			for (int i = 0; i < newObj.TimeOutFilter.Count; i++)
+				newObj.TimeOutFilter[i].CompiledSyntax = new Regex(newObj.TimeOutFilter[i].Syntax, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+			for (int i = 0; i < newObj.BanFilters.Count; i++)
+				newObj.BanFilters[i].CompiledSyntax = new Regex(newObj.BanFilters[i].Syntax, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+			return newObj;
 		}
 
-		public void Save()
-		{
-			string DirectoryPath = string.Format("Bot/Channels/{0}/", Channel);
-			string FilePath = DirectoryPath + "ChatFilters.xml";
-			XmlSerializer serializer = new XmlSerializer(typeof(ChatFilters));
-			if (!Directory.Exists(DirectoryPath))
-				Directory.CreateDirectory(DirectoryPath);
-			StreamWriter fw = new StreamWriter(FilePath);
-			serializer.Serialize(fw, this);
-			fw.Close();
-		}
+		public void Save() => XML_Utils.Save($"Bot/Channels/{Channel}/ChatFilters.xml", this);
 	}
 
 	[Serializable]
